3f837c983952887194bf42a7fc29158630e20fda
Diogo Ferreira <diogocraft.2@gmail.com>
2022-02-11 15:20:46 -0300

    :sparkles: adiionado ajuste na mensagem de inicialização REST da aplicação

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,4 +3,4 @@ import 'module-alias/register'
 import app from '@/main/config/app'
 import { env } from '@/main/config/env'
 
-app.listen(env.port, () => console.log(`Server is running at: http://localhost:${env.port}`))
+app.listen(env.port, () => console.log(`🚂 REST Server is running at: http://localhost:${env.port}`))
