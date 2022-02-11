47704d180263bcb495277c157fe70e5b72b50d2a
Diogo Ferreira <diogocraft.2@gmail.com>
2022-02-11 13:14:02 -0300

    :sparkles: implementada injeção de dependência do recurso para listagem de rankings

---
+++ src/main/factories/load-last-ranking-controller.ts
@@ -0,0 +1,12 @@
+import { LastRankingLoaderService } from '@/data/services'
+import { FakeRankingRepository } from '@/infra/repositories'
+import { LoadLastRankingController } from '@/presentation/controllers'
+import { Controller } from '@/presentation/contracts'
+
+export const makeLoadLastRankingController = (): Controller => {
+  const repo = new FakeRankingRepository()
+  const loader = new LastRankingLoaderService(repo)
+  const controller = new LoadLastRankingController(loader)
+
+  return controller
+}
